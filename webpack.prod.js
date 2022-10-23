c823b3bb1d171817d81462d3a9ee0348305720a4
hphat204 <pphat620@gmail.com>
2022-10-23 12:07:26 +0700

    minimize css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,18 +1,24 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 module.exports = {
   mode: "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "index.[hash].js",
+    filename: "index.[contenthash].js",
     assetModuleFilename: "imgs/[name][ext]",
+    clean: true,
+  },
+  optimization: {
+    minimizer: [`...`, new CssMinimizerWebpackPlugin()],
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
       {
         test: /\.html$/,
@@ -27,6 +33,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
     }),
   ],
 };
